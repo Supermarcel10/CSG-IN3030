9ba6f49e8d1deb29fd5f13818bcfef2125e9d84b
Supermarcel10 <marcelbarlik@gmail.com>
2024-12-13 00:42:45 +0000

    - Refactored all names to simplified versions as shown in lectures. - Added stroke. - Linked data sets with lookup. - Added tooltip. - Added TODOs.

--- src/Project.elm
+++ src/Project.elm
@@ -12,34 +12,62 @@ dataCsv : String
 dataCsv = "https://media.githubusercontent.com/media/Supermarcel10/CSG-IN3030/refs/heads/main/data/minified-RM080-2021-1.csv"
 
 
+-- TODO: Search through the data to see why so many authorities are missing
+-- TODO: Add zooming interaction
+-- TODO: Add selection
 
 -- GRAPHS
 ukChoropleth : Spec
 ukChoropleth =
   let
-    geographyData =
+    geoData =
       dataFromUrl topoJson
       [ topojsonFeature "lad" ]
 
-    projectionSettings =
+    csvData =
+      dataFromUrl dataCsv []
+
+    trans =
+      transform
+        << lookup "id"
+          csvData
+          "authority_id"
+          (luFields
+            [ "authority_name"
+            , "english_proficiency"
+            , "industry"
+            , "qualification"
+            , "deprivation"
+            , "count"
+            ])
+
+    proj =
       projection
         [ prType transverseMercator
         , prRotate 2 0 0
         ]
 
-    encodingSettings =
+    enc =
       encoding
       << color
-        [ mName "id"
+        [ mName "count"
         , mNominal
         , mLegend []
+        ]
+      << tooltip
+        [ tName "id"
+        , tNominal
         ]
   in
   toVegaLite
     [ width 800
     , height 1000
-    , geographyData
-    , projectionSettings
-    , encodingSettings []
-    , geoshape []
+    , geoData
+    , trans []
+    , proj
+    , enc []
+    , geoshape
+      [ maStroke "white"
+      , maStrokeWidth 0.1
+      ]
     ]

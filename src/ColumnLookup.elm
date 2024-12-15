4616e7c1281a17f62a2eec9ea10d86d209158114
Supermarcel10 <marcelbarlik@gmail.com>
2024-12-15 14:30:35 +0000

    - Added remaining graphs. - Updated ColumnLookup to use methods specific to Elm-Vegalite (since it cannot access Elm methods by the looks of things).

--- src/ColumnLookup.elm
+++ src/ColumnLookup.elm
@@ -1,45 +1,65 @@
 module ColumnLookup exposing (..)
 
+import VegaLite exposing (..)
 
 
-getEnglishProficiency : Int -> Maybe String
-getEnglishProficiency value =
-    case value of
-        1 -> Just "Main language is English (English or Welsh in Wales)"
-        2 -> Just "Main language is not English (English or Welsh in Wales)"
-        3 -> Just "Main language is not English (English or Welsh in Wales): Cannot speak English or cannot speak English well"
-        _ -> Nothing
 
+calculateAsQualificationLabel : List LabelledSpec -> List LabelledSpec
+calculateAsQualificationLabel =
+  calculateAs
+    """
+    datum.qualification == 0 ? 'No qualifications' :
+    datum.qualification == 1 ? 'Level 1' :
+    datum.qualification == 2 ? 'Level 2' :
+    datum.qualification == 3 ? 'Level 3' :
+    datum.qualification == 4 ? 'Level 4' :
+    'Other qualifications'
+    """
+    "qualification_label"
 
-getIndustry : Int -> Maybe String
-getIndustry value =
-    case value of
-        1 -> Just "A, B, D, E Agriculture, energy and water"
-        2 -> Just "C Manufacturing"
-        3 -> Just "F Construction"
-        4 -> Just "G, I Distribution, hotels and restaurants"
-        5 -> Just "H, J Transport and communication"
-        6 -> Just "K, L, M, N Financial, real estate, professional and administrative activities"
-        7 -> Just "O, P, Q Public administration, education and health"
-        8 ->  Just "R, S, T, U Other"
-        _ -> Nothing
+calculateAsIndustryCode : List LabelledSpec -> List LabelledSpec
+calculateAsIndustryCode =
+  calculateAs
+    """
+    datum.industry == 1 ? 'A, B, D, E' :
+    datum.industry == 2 ? 'C' :
+    datum.industry == 3 ? 'F' :
+    datum.industry == 4 ? 'G, I' :
+    datum.industry == 5 ? 'H, J' :
+    datum.industry == 6 ? 'K, L, M, N' :
+    datum.industry == 7 ? 'O, P, Q' :
+    'R, S, T, U, Other'
+    """
+    "industry_code"
 
+calculateAsIndustryLabel : List LabelledSpec -> List LabelledSpec
+calculateAsIndustryLabel =
+  calculateAs
+    """
+    datum.industry == 1 ? 'Agriculture, Energy & Water' :
+    datum.industry == 2 ? 'Manufacturing' :
+    datum.industry == 3 ? 'Construction' :
+    datum.industry == 4 ? 'Distribution & Hospitality' :
+    datum.industry == 5 ? 'Transport & Communication' :
+    datum.industry == 6 ? 'Financial, Real Estate, Professional & Administrative Activities' :
+    datum.industry == 7 ? 'Public Administration, Education & Health' :
+    'Other'
+    """
+    "industry_label"
 
-getQualification : Int -> Maybe String
-getQualification value =
-    case value of
-        0 -> Just "No qualifications"
-        1 -> Just "Level 1 and entry level qualifications: 1 to 4 GCSEs grade A* to C, Any GCSEs at other grades, O levels or CSEs (any grades), 1 AS level, NVQ level 1, Foundation GNVQ, Basic or Essential Skills"
-        2 -> Just "Level 2 qualifications: 5 or more GCSEs (A* to C or 9 to 4), O levels (passes), CSEs (grade 1), School Certification, 1 A level, 2 to 3 AS levels, VCEs, Intermediate or Higher Diploma, Welsh Baccalaureate Intermediate Diploma, NVQ level 2, Intermediate GNVQ, City and Guilds Craft, BTEC First or General Diploma, RSA Diploma"
-        3 -> Just "Level 3 qualifications: 2 or more A levels or VCEs, 4 or more AS levels, Higher School Certificate, Progression or Advanced Diploma, Welsh Baccalaureate Advance Diploma, NVQ level 3; Advanced GNVQ, City and Guilds Advanced Craft, ONC, OND, BTEC National, RSA Advanced Diploma"
-        4 -> Just "Level 4 qualifications or above: degree (BA, BSc), higher degree (MA, PhD, PGCE), NVQ level 4 to 5, HNC, HND, RSA Higher Diploma, BTEC Higher level, professional qualifications (for example, teaching, nursing, accountancy)"
-        5 -> Just "Other: apprenticeships, vocational or work-related qualifications, other qualifications achieved in England or Wales, qualifications achieved outside England or Wales (equivalent not stated or unknown)"
-        _ ->  Nothing
+calculateAsProficiencyLabel : List LabelledSpec -> List LabelledSpec
+calculateAsProficiencyLabel =
+  calculateAs
+    """
+    datum.english_proficiency == 1 ? 'Main language is English' :
+    datum.english_proficiency == 2 ? 'Main language not English: Can speak English very well' :
+    'Main language not English: Cannot speak English well or at all'
+    """
+    "proficiency_label"
 
-
-getDeprivation : Int -> Maybe String
+getDeprivation : Int -> String
 getDeprivation value =
     case value of
-        0 -> Just "Household is not deprived in the employment dimension"
-        1 -> Just "Household is deprived in the employment dimension"
-        _ -> Nothing
+        0 -> "Household is not deprived in the employment dimension"
+        1 -> "Household is deprived in the employment dimension"
+        _ -> ""
--- src/Project.elm
+++ src/Project.elm
@@ -1,7 +1,7 @@
-module Project exposing (ukChoropleth)
+module Project exposing (ukChoropleth, englishProfPie, industryQualificationBars, deprivationStats)
 
 import VegaLite exposing (..)
-
+import ColumnLookup exposing (..)
 
 
 -- DATA
@@ -13,7 +13,6 @@ dataCsv = "https://media.githubusercontent.com/media/Supermarcel10/CSG-IN3030/re
 
 
 -- TODO: Search through the data to see why so many authorities are missing
--- TODO: Add zooming interaction
 -- TODO: Add selection
 
 -- GRAPHS
@@ -22,13 +21,13 @@ ukChoropleth =
   let
     cfg =
       configure
-        << configuration (coBackground "#06070E")
+      << configuration (coBackground "#06070E")
 
     ps =
       params
-        << param "scale" [ paValue (num 8000), paBind (ipRange [ inName "Scale", inMin 8000, inMax 40000, inStep 1000 ]) ]
-        << param "cenLambda" [ paValue (num -1), paBind (ipRange [ inName "Center λ", inMin -3.7, inMax 0.5, inStep 0.05 ]) ]
-        << param "cenPhi" [ paValue (num 53), paBind (ipRange [ inName "Center 𝜙", inMin 50.5, inMax 55.5, inStep 0.05 ]) ]
+      << param "scale" [ paValue (num 8000), paBind (ipRange [ inName "Scale", inMin 8000, inMax 40000, inStep 1000 ]) ]
+      << param "cenLambda" [ paValue (num -1), paBind (ipRange [ inName "Center λ", inMin -3.7, inMax 0.5, inStep 0.05 ]) ]
+      << param "cenPhi" [ paValue (num 53), paBind (ipRange [ inName "Center 𝜙", inMin 50.5, inMax 55.5, inStep 0.05 ]) ]
 
     geoData =
       dataFromUrl topoJson
@@ -39,17 +38,17 @@ ukChoropleth =
 
     trans =
       transform
-        << lookup "id"
-          csvData
-          "authority_id"
-          (luFields
-            [ "authority_name"
-            , "english_proficiency"
-            , "industry"
-            , "qualification"
-            , "deprivation"
-            , "count"
-            ])
+      << lookup "id"
+        csvData
+        "authority_id"
+        (luFields
+          [ "authority_name"
+          , "english_proficiency"
+          , "industry"
+          , "qualification"
+          , "deprivation"
+          , "count"
+          ])
 
     proj =
       projection
@@ -85,3 +84,226 @@ ukChoropleth =
       , maFill "#0C0E1D"
       ]
     ]
+
+
+englishProfPie : Spec
+englishProfPie =
+  let
+  -- TODO: Add interaction with this
+      desiredAuthority = "E06000001"
+
+      cfg =
+        configure
+        << configuration (coView [ vicoStroke Nothing ])
+        << configuration (coBackground "#06070E")
+
+      data =
+        dataFromUrl dataCsv []
+
+      trans =
+        transform
+        << filter (fiExpr ("datum.authority_id == '" ++ desiredAuthority ++ "'"))
+        << calculateAsProficiencyLabel
+
+      enc =
+        encoding
+        << position Theta [ pName "count", pQuant ]
+        << color
+          [ mName "proficiency_label"    
+          , mLegend []
+          , mScale [ scRange (raStrs ["#2a9d8f", "#e9c46a", "#e76f51"]) ]
+          ]
+        << tooltip
+          [ tName "proficiency_label"
+          , tNominal
+          ]
+    in
+    toVegaLite
+    [ cfg []
+    , data
+    , trans []
+    , enc []
+    , arc [ maStrokeWidth 0 ]
+    ]
+
+
+industryQualificationBars : Spec
+industryQualificationBars =
+  let
+  -- TODO: Add interaction with this
+    desiredAuthority = "E06000001"
+
+    cfg =
+      configure
+      << configuration (coBackground "#06070E")
+      << configuration (coTitle [ ticoColor "white" ])
+      << configuration (coView [ vicoStroke Nothing ])
+      << configuration (coAxis [ axcoTitleColor "white", axcoLabelColor "white" ])
+      << configuration (coLegend [ lecoTitleColor "white", lecoLabelColor "white" ])
+      << configuration (coHeader [ hdLabelColor "white" ])
+
+    trans =
+      transform
+      << filter (fiExpr ("datum.authority_id == '" ++ desiredAuthority ++ "'"))
+      << calculateAs 
+        """
+        datum.qualification == 0 ? 'No qualifications' :
+        datum.qualification == 1 ? 'Level 1' :
+        datum.qualification == 2 ? 'Level 2' :
+        datum.qualification == 3 ? 'Level 3' :
+        datum.qualification == 4 ? 'Level 4' :
+        'Other qualifications'
+        """
+        "qualification_label"
+      << calculateAs
+        """
+        datum.industry == 1 ? 'A, B, D, E' :
+        datum.industry == 2 ? 'C' :
+        datum.industry == 3 ? 'F' :
+        datum.industry == 4 ? 'G, I' :
+        datum.industry == 5 ? 'H, J' :
+        datum.industry == 6 ? 'K, L, M, N' :
+        datum.industry == 7 ? 'O, P, Q' :
+        'R, S, T, U, Other'
+        """
+        "industry_code"
+      << calculateAs
+        """
+        datum.industry == 1 ? 'Agriculture, Energy & Water' :
+        datum.industry == 2 ? 'Manufacturing' :
+        datum.industry == 3 ? 'Construction' :
+        datum.industry == 4 ? 'Distribution & Hospitality' :
+        datum.industry == 5 ? 'Transport & Communication' :
+        datum.industry == 6 ? 'Financial, Real Estate, Professional & Administrative Activities' :
+        datum.industry == 7 ? 'Public Administration, Education & Health' :
+        'Other'
+        """
+        "industry_label"
+      << joinAggregate 
+        [ opAs opSum "count" "industry_total" ]
+        [ wiGroupBy [ "industry_label" ] ]
+      << calculateAs "datum.count/datum.industry_total * 100" "percentage"
+
+    sortOrder =
+      soCustom 
+        ( strs 
+          [ "No qualifications"
+          , "Level 1"
+          , "Level 2"
+          , "Level 3"
+          , "Level 4"
+          , "Other qualifications"
+          ]
+        )
+
+    data =
+      dataFromUrl dataCsv []
+
+    enc =
+      encoding
+      << position X [ pName "count", pAggregate opSum, pTitle "Count" ]
+      << position Y
+        [ pName "industry_label"
+        , pTitle "Industry"
+        , pSort [ sortOrder ]
+        ]
+      << color
+        [ mName "industry_code"
+        , mTitle "Qualifications"
+        , mScale [ scRange (raStrs ["#e76f51", "#f4a261", "#e9c46a", "#2a9d8f", "#287271", "#aaaaaa"]) ]
+        , mSort [ sortOrder ]
+        ]
+      << tooltips
+        [ [ tName "qualification_label", tTitle "Qualification" ]
+        , [ tName "count", tAggregate opSum, tTitle "Count" ]
+        , [ tName "percentage", tFormat ".1f", tTitle "% of Industry" ]
+        ]
+  in
+  toVegaLite
+  [ data
+  , cfg []
+  , trans []
+  , enc []
+  , bar []
+  ]
+
+
+deprivationStats : Spec
+deprivationStats =
+    let
+        desiredAuthority = "E06000001"
+
+        -- Calculate total percentage in deprivation
+        overallTrans =
+          transform
+          << filter (fiExpr ("datum.authority_id == '" ++ desiredAuthority ++ "'"))
+          << aggregate
+            [ opAs opSum "count" "total_count" ]
+            [ "deprivation" ]
+          << joinAggregate
+            [ opAs opSum "total_count" "grand_total" ]
+            [ wiGroupBy [] ]
+          << filter (fiExpr "datum.deprivation == 1")
+          << calculateAs "datum.total_count / datum.grand_total * 100" "percent_deprived"
+
+
+        -- Transform for industry breakdown
+        industryTrans =
+          transform
+          << filter (fiExpr ("datum.authority_id == '" ++ desiredAuthority ++ "'"))
+          << aggregate
+            [ opAs opSum "count" "industry_total" ]
+            [ "industry", "deprivation" ]
+          << joinAggregate
+            [ opAs opSum "industry_total" "total_per_industry" ]
+            [ wiGroupBy [ "industry" ] ]
+          << filter (fiExpr "datum.deprivation == 1")
+          << calculateAsIndustryCode
+          << calculateAsIndustryLabel
+          << calculateAs "datum.industry_total / datum.total_per_industry * 100" "percent_deprived"
+
+
+        -- Overall percentage text
+        textEnc =
+          encoding
+          << text [ tName "percent_deprived", tFormat ".2f" ]
+
+        textSpec =
+          asSpec
+            [ dataFromUrl dataCsv []
+            , overallTrans []
+            , textEnc []
+            , textMark [ maColor "white", maSize 48, maAlign haCenter, maBaseline vaMiddle ]
+            ]
+
+
+        -- Industry breakdown bars
+        barsEnc =
+          encoding
+          << position X [ pName "percent_deprived", pQuant, pTitle "% in Deprivation" ]
+          << position Y
+            [ pName "industry_code"
+            , pTitle "Industry"
+            ]
+          << color [ mStr "#e63946" ]
+          << tooltips
+            [ [ tName "industry_label", tTitle "Industry" ]
+            , [ tName "percent_deprived", tFormat ".2f", tTitle "% in Deprivation" ]
+            ]
+
+        barsSpec =
+          asSpec
+            [ dataFromUrl dataCsv []
+            , industryTrans []
+            , barsEnc []
+            , bar []
+            ]
+
+    in
+    toVegaLite
+      [ vConcat
+        [ textSpec
+        , barsSpec
+        ]
+      , standardConfig
+      ]
